091df86a3d4ca09419c1d771985ddc494c2bf3d8
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-10-31 23:53:03 +0700

    Add exp to jwt authorization

--- src/services/BaseConfig.js
+++ src/services/BaseConfig.js
@@ -4,6 +4,9 @@ import jsrsasign from "jsrsasign"
 export class BaseConfig {
     sessionUseCase = new SessionUseCase()
 
+    currentTime = Math.floor(Date.now() / 1000)
+    expTime = this.currentTime + 10
+
     noJwtConfig = {
         headers: {
             "Content-Type": "multipart/form-data"
@@ -13,7 +16,7 @@ export class BaseConfig {
     jwtConfig = {
         headers: {
             "Content-Type": "multipart/form-data",
-            "Authorization": "Bearer " + jsrsasign.jws.JWS.sign(null, { alg: 'HS256' }, this.sessionUseCase.get(), process.env.REACT_APP_SECRET_KEY)
+            "Authorization": "Bearer " + jsrsasign.jws.JWS.sign(null, { alg: 'HS256' }, { ...this.sessionUseCase.get(), exp: this.expTime }, process.env.REACT_APP_SECRET_KEY)
         }
     }
 }
