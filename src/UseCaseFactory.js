b1775b787f255217043ce8fdbede74e29360bac1
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-10-31 08:58:48 +0700

    Create current session use case

--- src/UseCaseFactory.js
+++ src/UseCaseFactory.js
@@ -1,6 +1,26 @@
 import { UserService } from "./services/UserService"
 
-// User Service
+// Session Use Case
+
+export class SessionUseCase {
+    get() {
+        return {
+            name: localStorage.getItem('name'),
+            username: localStorage.getItem('username'),
+            role: localStorage.getItem('role')
+        }
+    }
+    set(payload) {
+        localStorage.setItem('name', payload.name)
+        localStorage.setItem('username', payload.username)
+        localStorage.setItem('role', payload.role)
+    }
+    delete() {
+        localStorage.clear()
+    }
+}
+
+// User Use Case
 
 export class LoginUseCase {
     execute(loginReq) {
@@ -9,6 +29,8 @@ export class LoginUseCase {
 }
 
 export class UseCaseFactory {
-    // User Service
+    // Session Use Case
+    current() { return new SessionUseCase() }
+    // User Use Case
     userLogin() { return new LoginUseCase() }
 }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,14 +2,20 @@ import axios from "axios"
 import { from, map, of } from "rxjs"
 import { catchError } from "rxjs/operators"
 import { noJwtConfig } from "./BaseConfig"
+import { SessionUseCase } from "../UseCaseFactory"
 
 export class UserService {
+    sessionUseCase = new SessionUseCase()
+
     endpoint = process.env.REACT_APP_ENDPOINT + "/user"
 
     login(loginReq) {
         return from(axios.post(this.endpoint + "/login", loginReq, noJwtConfig))
         .pipe(
-            map((response) => response.data),
+            map((response) => {
+                this.sessionUseCase.set(response.data.output_schema)
+                return response.data
+            }),
             catchError((error) => {
                 return of(error.response.data)
             })
